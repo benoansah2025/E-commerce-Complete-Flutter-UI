965cdc33a1ebd6764e59b8253fbb30640ba6868e
memphisperla <memphisperla@gmail.com>
2025-08-05 14:43:03 +0000

    Upgraded gradle to 8.12 and fixed petty issues within code

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,19 @@ void main() {
   runApp(const MyApp());
 }
 
-// Thanks for using our template. You are using the free version of the template.
-// 🔗 Full template: https://theflutterway.gumroad.com/l/fluttershop
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shop Template by The Flutter Way',
       theme: AppTheme.lightTheme(context),
-      // Dark theme is inclided in the Full template
+      // Dark theme is included in the Full template
       themeMode: ThemeMode.light,
       onGenerateRoute: router.generateRoute,
       initialRoute: onbordingScreenRoute,
